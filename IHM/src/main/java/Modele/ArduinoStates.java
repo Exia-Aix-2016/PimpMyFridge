2e2fe3124cd0034aa573c72f6252c4439f86e333
Unknown <mandel.vaubourg@viacesi.fr>
2017-11-21 11:08:56 +0100

    Edit Model

--- IHM/src/main/java/Modele/ArduinoStates.java
+++ IHM/src/main/java/Modele/ArduinoStates.java
@@ -1,4 +1,69 @@
 package Modele;
 
+import javafx.beans.property.DoubleProperty;
+import javafx.fxml.FXML;
+
 public class ArduinoStates {
+
+    //Valeur mesure
+    private DoubleProperty temperaturePlaque;
+    private DoubleProperty temperatureAmbiante;
+    private DoubleProperty pointRosee;
+
+    //PID
+    private DoubleProperty coefP;
+    private DoubleProperty coefI;
+    private DoubleProperty coefD;
+
+    public double getPointRosee() {
+        return pointRosee.get();
+    }
+
+    public double getTemperatureAmbiante() {
+        return temperatureAmbiante.get();
+    }
+
+    public double getTemperaturePlaque() {
+        return temperaturePlaque.get();
+    }
+
+    public double getCoefD() {
+        return coefD.get();
+    }
+
+    public double getCoefI() {
+        return coefI.get();
+    }
+
+    public double getCoefP() {
+        return coefP.get();
+    }
+
+    public void setCoefD(double coefD) {
+        this.coefD.set(coefD);
+    }
+
+    public void setCoefI(double coefI) {
+        this.coefI.set(coefI);
+    }
+
+    public void setCoefP(double coefP) {
+        this.coefP.set(coefP);
+    }
+
+    public void setPointRosee(double pointRosee) {
+        this.pointRosee.set(pointRosee);
+    }
+
+    public void setTemperatureAmbiante(double temperatureAmbiante) {
+        this.temperatureAmbiante.set(temperatureAmbiante);
+    }
+
+    public void setTemperaturePlaque(double temperaturePlaque) {
+        this.temperaturePlaque.set(temperaturePlaque);
+    }
+
+    public DoubleProperty pointRoseeProperty() {
+        return pointRosee;
+    }
 }
--- IHM/src/main/java/ihm/AppController.java
+++ IHM/src/main/java/ihm/AppController.java
@@ -1,15 +1,21 @@
 package ihm;
 
+import Modele.ArduinoStates;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.TextField;
 
+import java.util.Observable;
+import java.util.Observer;
 
 
+/**
+ * Controller de l'application
+ */
+public class AppController implements Observer {
 
-public class AppController {
-
+    private ArduinoStates arduinoStates;
 
     @FXML
     private TextField pro;
@@ -29,19 +35,48 @@ public class AppController {
     @FXML
     public void initialize() {
 
-        consigne.textProperty().addListener((obs,old,niu)->{
+        //Création du modèle arduino.
+        this.arduinoStates = new ArduinoStates();
+
+
+
+
+
+
+
+
+        //LISTERNERS
+
+        this.consigne.textProperty().addListener((obs,old,niu)->{
             System.out.println(niu);
 
 
         });
+
+        this.pro.textProperty().addListener((obs,old,niu)->{
+            System.out.println(niu);
+
+
+        });
+
+        this.deriv.textProperty().addListener((obs,old,niu)->{
+            System.out.println(niu);
+
+
+        });
+
+        this.integ.textProperty().addListener((obs,old,niu)->{
+            System.out.println(niu);
+
+
+        });
+
     }
 
 
 
+    @Override
+    public void update(Observable o, Object arg) {
 
-
-
-
-
-
+    }
 }
