c3c17ed83b5d09c9ca1f85a4b5090a3f939755f1
Unknown <mandel.vaubourg@viacesi.fr>
2017-11-22 10:52:49 +0100

    add State system

--- IHM/src/main/java/Modele/ArduinoStates.java
+++ IHM/src/main/java/Modele/ArduinoStates.java
@@ -1,12 +1,51 @@
 package Modele;
 
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.SimpleDoubleProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
-import javafx.fxml.FXML;
+import java.util.*;
 
 
-public class ArduinoStates {
+public class ArduinoStates implements Observer{
 
+    private Stack<State> stateHistory;
+
+    public ArduinoStates(){
+        this.stateHistory = new Stack<>();
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        this.addState((HashMap<String, String>) arg);
+    }
+
+    private boolean addState(final HashMap<String, String> serial){
+
+        //Test empty zone in hash
+        for (String key : serial.keySet()) {
+
+            if(serial.get(key).isEmpty()){
+                return false;
+
+            }
+        }
+
+
+        State state = new State(Double.parseDouble(serial.get("Ta")),
+                                Double.parseDouble(serial.get("Tp")),
+                                Double.parseDouble(serial.get("H")),
+                                Double.parseDouble(serial.get("Pr")),
+                                Double.parseDouble(serial.get("Pw")),
+                                Double.parseDouble(serial.get("consigneTemp")),
+                                Double.parseDouble(serial.get("Kp")),
+                                Double.parseDouble(serial.get("Ki")),
+                                Double.parseDouble(serial.get("Kd")));
+
+        return true;
+    }
+
+    public final State getState(){
+        return this.stateHistory.peek();
+    }
+    @org.jetbrains.annotations.Contract(pure = true)
+    public final State getState(final int index){
+        return this.stateHistory.get(index);
+    }
 }
--- IHM/src/main/java/Modele/State.java
+++ IHM/src/main/java/Modele/State.java
@@ -1,5 +1,41 @@
 package Modele;
 
-public class State {
+import javafx.beans.property.DoubleProperty;
 
+import java.time.*;
+ public class State {
+
+
+    private double Ta;//Température ambiante
+    private double Tp;//Température plaque
+    private double H;//Humidité
+    private double Pr;//Point de rosée
+    private double Pw;//Puissance envoyé
+
+     //Ordre
+     private double consigneTemp;
+     private double Kp;
+     private double Ki;
+     private double Kd;
+    private Instant time;//instant de l'état.
+
+
+    public State(final double Ta, final double Tp, final double H, final double Pr, final double Pw, final double consigneTemp,
+                 final double Kp, final double Ki, final double Kd){
+
+        this.Ta = Ta;
+        this.Tp = Tp;
+        this.H = H;
+        this.Pr = Pr;
+        this.Pw = Pw;
+
+        //Ordre.
+        this.consigneTemp = consigneTemp;
+        this.Kp = Kp;
+        this.Ki = Ki;
+        this.Kd = Kd;
+
+
+        this.time = Instant.now();
+    }
 }
